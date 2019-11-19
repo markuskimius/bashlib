777a63f82c3ca88876b0346ffafc5c1176ca79aa
Mark Kim <markuskimius@gmail.com>
2019-11-18 21:52:56 -0500

    mode.sh -- initial commit

---
+++ lib/bashlib/mode.sh
@@ -0,0 +1,21 @@
+##############################################################################
+# BASHLIB: BASH library framework
+# https://github.com/markuskimius/bashlib
+#
+# Copyright (c)2019 Mark K. Kim
+# Released under the Apache license 2.0
+# https://github.com/markuskimius/bashlib/blob/master/LICENSE
+##############################################################################
+
+function mode::strict() {
+    shopt -so nounset     # Do not allow reading of uninitialized variable
+    shopt -so errexit     # Exit script if any nonzero exit status is not caught
+    shopt -so pipefail    # Exit status of any command in a pipe chain is the exit status of the chain
+    shopt -so noclobber   # Do not allow exiting file to be redirected to
+}
+
+function mode::extended() {
+    shopt -s extglob    # Enable extended glob
+    shopt -s globstar   # ** expands to subdirectories
+}
+

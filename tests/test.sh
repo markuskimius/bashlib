fe24121842c6f5e4238b43649d6d9933df05fe45
Mark Kim <markuskimius@gmail.com>
2019-11-26 21:25:10 -0500

    test a specific library
    Test a specific library by passing the name of the library as an argument.

--- tests/test.sh
+++ tests/test.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
 
+include "bashlib/types.sh"
 include "bashlib/mode.sh"
 
 bashlib::mode::strict
 
-cd "$BASHLIB/lib"
+function main() {
+    bashlib::string regex=""
 
-for file in $(grep -l '__test__' */*.sh); do
-    include "$file"
+    if (( $# > 0 )); then
+        regex="$1"
+    fi
 
-    name=$(basename "$file" .sh)
+    cd "$BASHLIB/lib"
 
-    printf "%-32s" "Testing ${file}..."
-    bashlib::$name::__test__
-done
+    for file in $(grep -l '__test__' */*.sh); do
+        if [[ "$file" =~ "$regex" ]]; then
+            include "$file"
 
+            name=$(basename "$file" .sh)
+
+            printf "%-32s" "Testing ${file}..."
+            bashlib::$name::__test__
+        fi
+    done
+}
+
+main "$@"

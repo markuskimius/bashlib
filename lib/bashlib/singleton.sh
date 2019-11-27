17af0b34ed45a8631a8373d227812e709b500ba3
Mark Kim <markuskimius@gmail.com>
2019-11-26 21:36:23 -0500

    singleton.sh -- initial commit

---
+++ lib/bashlib/singleton.sh
@@ -0,0 +1,42 @@
+include "./types.sh"
+include "./class.sh"
+include "./function.sh"
+include "./exception.sh"
+
+function bashlib::singleton() {
+    bashlib::string class=$1 && shift
+    bashlib::string object
+
+    if bashlib::function::defined $class; then
+        bashlib::throw "$class is already defined"
+    fi
+
+    bashlib::class::instantiate $class object "$@"
+
+    source <(cat <<EOF
+        function $class () {
+            $object "\$@"
+        }
+EOF
+    )
+}
+
+function bashlib::singleton::__test__() {
+    function myclass::__constructor__() {
+        bashlib::string this=$1 && shift
+
+        $this __set__ value "$@"
+    }
+
+    function myclass::read() {
+        bashlib::string this=$1 && shift
+
+        $this __get__ value "$@"
+    }
+
+    bashlib::singleton myclass "Hello, world!"
+    [[ $(myclass read) == "Hello, world!" ]] || bashlib::throw
+
+    echo "[PASS]"
+}
+
